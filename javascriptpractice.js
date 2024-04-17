688f62260aeb8df54e4ba2a0693c43c300a91872
Leo Esmilaire <leo.chavesmilaire@gmail.com>
2024-04-17 12:02:21 +0200

    init

---
+++ javascriptpractice.js
@@ -0,0 +1,79 @@
+/* var person = "Leo"
+console.log("Hello", person)
+var greeting = "Hello"
+console.log(greeting, person)
+greeting = "Hi"
+person = "Rayan"
+console.log(greeting, person) */
+
+/*console.log(2 + 2)
+console.log(2 > 3)
+console.log(2 ==  3)
+console.log(2 < 3)
+123
+123.456
+2+2
+console.log(9%8) */
+
+/*console.log("Hello" + "there")
+console.log("Hello there")*/
+
+/*console.log(1<2)
+console.log(2<1)
+console.log(1==2)
+console.log( 1 != 2)
+var score = 100
+console.log(score)
+console.log(100 == "100")
+console.log(100 === "100")
+console.log(100 !== "100") */
+
+/*var currentTime = 10;
+console.log(currentTime > 9 && currentTime < 17)
+
+var currentTime = 7; 
+console.log(currentTime > 9 || currentTime > 17);
+
+var petHungry = true;
+console.log('Feeding the pet')
+console.log("Pet is hungry:", !petHungry);
+console.log(petHungry);
+petHungry = !petHungry
+console.log(petHungry)
+
+var mon = 1;
+var tue = 2;
+var wed = 1;
+var thu = 2;
+var fri = 3;
+console.log(mon + tue + wed + thu + fri);
+
+var overtime = 1;
+overtime += 3;
+console.log(overtime)
+
+var longString = "";
+longString += "My dead nan"
+longString += " coocoo "
+longString += "is amazing..."
+console.log(longString)*/
+
+var result = 50;
+
+if(result > 40) {
+    console.log('You passed the test')    
+} else {
+    console.log('Unfortunately you did not pass')
+}
+
+var place = "second";
+
+if(place == "first") {
+    console.log("Gold")
+} else if (place == "second") {
+    console.log("Silver")
+} else if (place == "third") {
+    console.log("Bronze")
+} else {
+    console.log("No medal")
+}
