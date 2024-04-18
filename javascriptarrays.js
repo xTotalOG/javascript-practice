52c6d58b23cf47e0fec5a6d14ab87ec08b173e7f
Leo Esmilaire <leo.chavesmilaire@gmail.com>
2024-04-18 16:37:09 +0200

    iterables

--- javascriptarrays.js
+++ javascriptarrays.js
@@ -1,7 +1,7 @@
 /*var train1 = ["wheat", "barley", "potato", "salt", "rocks"];
 console.log(train1[0]);*/
 
-function letterFinder(word, match){
+/*function letterFinder(word, match){
     for (var i = 0; i < word.length; i++){
         if (word[i] == match){
             console.log("Found the", match, "at", i)
@@ -11,4 +11,33 @@ function letterFinder(word, match){
     }
 }
 
-letterFinder("test","t")
+letterFinder("test","t")*/
+
+// arrays are iterable
+var veggies = ["onion", "parsley", "carrot"];
+console.log(veggies.length);
+
+console.log(veggies[0]);
+console.log(veggies[1]);
+
+for (var i = 0; i < veggies.length; i++){
+    console.log(veggies[i]);
+} 
+
+// strings are iterable as well
+var greeting = "Howdy";
+console.log(greeting.length);
+
+console.log(greeting[0]);
+console.log(greeting[1]);
+
+for (var i = 0; i < greeting.length; i++){
+    console.log(greeting[i]);
+}
+
+// similar but strings and arrays are not the same
+var greet = "Hello ";
+var user = "Lisa";
+
+console.log(greet+user);
+console.log(greet.concat(user));
