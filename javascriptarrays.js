716c512593cbfd078ee08ab38ac0454422de4221
Leo Esmilaire <leo.chavesmilaire@gmail.com>
2024-04-18 11:34:33 +0200

    new practice

--- javascriptarrays.js
+++ javascriptarrays.js
@@ -1,2 +1,14 @@
-var train1 = ["wheat", "barley", "potato", "salt", "rocks"];
-console.log(train1[0]);
+/*var train1 = ["wheat", "barley", "potato", "salt", "rocks"];
+console.log(train1[0]);*/
+
+function letterFinder(word, match){
+    for (var i = 0; i < word.length; i++){
+        if (word[i] == match){
+            console.log("Found the", match, "at", i)
+        } else {
+            console.log ("---No match found at", i)
+        }
+    }
+}
+
+letterFinder("test","t")
---
+++ javascriptobject.js
@@ -0,0 +1,7 @@
+var love = {
+    pain: "high",
+    joy: "endless",
+    risk: "high",
+}
+
+console.log(love.joy)
